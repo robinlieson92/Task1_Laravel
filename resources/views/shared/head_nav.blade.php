2b1461f354c45f7989587164fa0ed12d8d4e2038
=Robin Lieson <=robinlieson92@gmail.com>
2016-11-14 10:32:52 +0700

    Modified Navbar with Responsive

--- resources/views/shared/head_nav.blade.php
+++ resources/views/shared/head_nav.blade.php
@@ -1,11 +1,20 @@
 <div class="navbar navbar-default">
+ <div class="container-fluid">
 	<div class="navbar-header">
+		<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			        <span class="icon-bar"></span>
+			    </button>
        	{!! link_to(route('.home'),"Training Bootstrap", ["class"=>"navbar-brand"]) !!}
         
     </div>
-    <ul class="nav navbar-nav navbar-right">
+    <div class="navbar-collapse collapse navbar-responsive-collapse">
+	    <ul class="nav navbar-nav navbar-right">
 			<li>{!! link_to(route('.home'),"Home") !!}</li>
 			<li>{!! link_to(route('.profile'),"Profile") !!}</li>
 			<li>{!! link_to(route('.contact'),"Contact") !!}</li>
-			</ul>
+		</ul>
+	</div>
+ </div>
 </div>
